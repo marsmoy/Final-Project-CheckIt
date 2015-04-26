61d6d68e29ac19ab5b5d27312d7fda04e095f571
Jack Flaherty <thepacific@me.com>
2015-04-26 13:02:10 -0400

    created home page and some backend db stuff

---
+++ include/checkit_ops.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>MySQL from PHP</title>
+</head>
+<body>
+
+<?php
+
+	$dbc= @mysqli_connect("localhost", "bowditcw", "H8zFAA2E", "bowditcw") or
+					die("Connect failed3: ". mysqli_connect_error());
+					
+	
+	
+	
+	
+	if ( $_POST['first'] != null ) {
+		$first = $_POST['first'];
+		$last = $_POST['last'];
+		
+		if ( $_POST['email'] != null ) {
+			$email = $_POST['email'];
+			
+			$email_query = "select email from checkit where email = '$email'";
+			$result = mysqli_query($dbc, $email_query) or die("bad query".mysqli_error($dbc));	
+			$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+			$email1 = $row['email'];
+			if( $email1 == null ) {
+				if ( $_POST['password'] != null) {
+					$password = $_POST['password'];
+					
+					if ( $_POST['password2'] != null) {
+						$password2 = $_POST['password2'];
+						
+						if ($password == $password2) {
+							handleform($dbc,$first,$last,$email,sha1($password));
+						}
+						else echo "Passwords don't match";
+					}
+					else echo 'No Password2 Entered';
+					
+				}
+				else echo 'No Password Entered';
+			}
+			else echo 'Email Already Exists';
+		}
+		else echo 'No Email Entered';
+	}
+	else echo 'No Name Entered';
+	echo "<br><br>";
+	echo "<a href='http://cscilab.bc.edu/~bowditcw/checkit'>Back to Home Page</a>";
+?>
+
+</body>
+</html>
+<?php
+function performQuery($dbc, $query){
+	$result = mysqli_query($dbc, $query) or die("bad query".mysqli_error($dbc));
+	return $result;
+}
+
+
+function handleform($dbc,$first,$last,$email,$password){
+	$query = "INSERT INTO checkit VALUES ('$first','$last','0.0','', '$password','$email')";
+	$result = performQuery($dbc, $query);
+	if ( ! $result )
+		echo "<br>Oops! Something went wrong";
+	else
+		echo "<br>Congratulations on joining CheckIt!";
+	echo "<a href='http://cscilab.bc.edu/~bowditcw/checkit'>Back to Home Page</a>";
+
+}
+?>
--- index.php
+++ index.php
@@ -10,9 +10,202 @@
 <head>
      <meta charset="utf-8" />
      <title>CheckIt</title>
+  	 <script type="text/javascript">
+        function emailValidate(){
+			var theemail = document.getElementById("email1").value ;
+			if (theemail.length < 1) {
+				var errorrpt=document.getElementById("email1error");
+				errorrpt.innerHTML = "Please enter an email";
+				return false;
+			} 
+			var errorrpt=document.getElementById("email1error");
+			errorrpt.innerHTML = "";
+	
+			return true;
+		}
+     
+     	function validate(){
+			var validFirst = validateFirst();
+       		var validLast = validateLast();
+			var validEmail = validateEmail();
+			var validPassword = validatePassword();
+			
+			if (!validFirst) return false;
+       		if (!validLast) return false;
+			if (!validEmail) return false;
+			if (!validPassword) return false;
+			return true;
+		}
+
+		function validateFirst(){
+			var thename= document.getElementById("first").value ;
+			
+			if (thename.length < 1) {
+				var errorrpt=document.getElementById("firsterror");
+				errorrpt.innerHTML = "Please enter a first name";
+				return false;
+			} 
+			var errorrpt=document.getElementById("firsterror");
+			errorrpt.innerHTML = "";
+	
+			return true;
+		}
+		
+		function validateLast(){
+			var thename= document.getElementById("last").value ;
+			
+			if (thename.length < 1) {
+				var errorrpt=document.getElementById("lasterror");
+				errorrpt.innerHTML = "Please enter a last name";
+				return false;
+			} 
+			var errorrpt=document.getElementById("lasterror");
+			errorrpt.innerHTML = "";
+	
+			return true;
+		}
+		
+		function validateEmail(){
+			var theemail= document.getElementById("email").value ;
+			var emailregex=/^[A-Za-z0-9]{1}\\w*@[a-zA-Z]{1}\\w*\\.(com|gov|edu)$/;
+			
+			if (!emailregex.test(theemail)) {
+				var errorrpt=document.getElementById("emailerror");
+				errorrpt.innerHTML = "Please enter a valid email";
+				return false;
+			} 
+			var errorrpt=document.getElementById("emailerror");
+			errorrpt.innerHTML = "";
+
+			return true;
+		}
+		
+		function validatePassword(){
+			var pass1= document.getElementById("password").value ;
+			var pass2= document.getElementById("password2").value ;
+			
+			if (pass1 != pass2) {
+				var errorrpt=document.getElementById("passerror");
+				errorrpt.innerHTML = "Passwords Don't Match";
+				return false;
+			} 
+		    if (pass1.length < 1) {
+				var errorrpt1=document.getElementById("pass1error");
+				errorrpt1.innerHTML = "Please enter a password";
+				return false;
+			} 
+			if (thepass2.length < 1) {
+				var errorrpt=document.getElementById("passerror");
+				errorrpt.innerHTML = "Please enter a password";
+				return false;
+			} 
+			var errorrpt=document.getElementById("passerror");
+			errorrpt.innerHTML = "";
+			var errorrpt1=document.getElementById("pass1error");
+			errorrpt1.innerHTML = "";
+	
+			return true;
+		}
+      </script>
 </head>
 <body>
-	Hello World!<br>
-	Love, CheckIt
+
+	<?php
+       	displayHome();
+		displaynewAccountForm();
+		forgotPasswordForm();
+	?>
 </body>
 </html>
+<?php
+    function displayHome(){
+?>
+		<h1>CheckIt Stock Portoflio</h1>
+		<form method="get">
+			<input type="submit" name="about" value="About CheckIt">
+		</form>
+		<form method="get" action="cscilab.bc.edu/~oconnonx/CheckIt/profile.php">
+			<input type="text" name="user" value="username">
+			<input type="text" name="pw" value="password">
+			<input type="submit" name="login" value="Login">
+		</form>
+  
+	<!--RSS below -->
+
+	<?php
+	//initialize news source
+
+	$rss_feed = "http://rss.nytimes.com/services/xml/rss/nyt/Business.xml";
+
+	$rss= new SimpleXMLElement(file_get_contents($rss_feed));
+	$title = $rss->channel->title;
+	echo "<h1>$title</h1>";
+	$items = $rss->channel->item;
+		foreach ($items as $item) {
+			echo "<div class='news'>
+			<h2>$item->title</h2>\n";
+			echo '<a href="' . $item->link . '">' . $item->title . '</a><br>';
+			echo $item->description . "<br><br>\n";
+			echo "<br></div>";
+		}
+	}  
+      
+    function forgotPasswordForm(){
+?>
+		<fieldset><legend><h4>Enter Information for New Password</h4></legend>
+			<br><br>
+			<form method = "get" action = "include/checkit_password.php" onsubmit = "return emailValidate();">
+				<label for="email">Please enter your email: </label>
+				<input type = "text" id = "email1" name = "email1">
+				<span class="ereport" id="email1error"></span>
+				<br><br>
+				<input type = "submit"  name = "submit_button" value = "Email New Password" >
+			</form>
+		</fieldset>
+		<?php
+}
+
+    function displaynewAccountForm(){
+      ?>
+        <fieldset><legend><h4>Create Checkit Account</h4></legend>
+          <br><br>
+          <form method="post" action = "include/checkit_ops.php" onsubmit = "return validate();">
+              <label for="first">Please enter your first name: </label>
+              <input type = "text" id = "first" name = "first">
+				<span class="ereport" id="firsterror"></span>
+            	<br><br>
+            	<label for="name">Please enter your last name: </label>
+				<input type = "text" id = "last" name = "last">
+				<span class="ereport" id="lasterror"></span>
+				<br><br>
+				<label for="email">Please enter your email: </label>
+				<input type = "text" id = "email" name = "email">
+				<span class="ereport" id="emailerror"></span>
+				<br><br>
+				<label for="password">Please enter your password: </label>
+				<input type = "password" id = "password" name = "password">
+				<span class="ereport" id="pass1error"></span>
+				<br><br>
+				<label for="password2">Please confirm your password: </label>
+				<input type = "password" id = "password2" name = "password2">
+				<span class="ereport" id="passerror"></span>
+				<br><br>
+    			<input type = "submit"  name = "submit_button" value = "Submit">
+           </form>
+         </fieldset>
+<?php
+}
+function displayAbout(){
+?>
+	<div>CheckIt is a simple idea. Unlike the messy, intimidating web pages<br>
+	one will find on big banking websites meant to confuse and dazzle<br>
+	so they can steal your money, CheckIt is simple!<br><br>
+	Create your account, add your stocks. Once done, CheckIt will serve<br>
+	two basic functions for you. Buy, sell, and research. You can <br>
+	search prices, view the big indices, and keep track of your total value.<br>
+	I, for one, CheckIt every day!<br><br>
+	Copyright 2015 Bowditch, Chu, Flaherty & co.</div>
+<?php
+}
+
+?>
